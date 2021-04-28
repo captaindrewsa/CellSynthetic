ba62d6156d856eed61b24402b3c591c41588777c
Andrew Vlasov <captaindrewsa@gmail.com>
2021-04-28 12:36:00 +0200

    First commit in main

---
+++ main.py
@@ -0,0 +1,14 @@
+class Compound():
+
+    def __init__(self, name, graph):
+        self.NAME = name
+        self.FORMULA = graph
+    
+    def GetFormula(self):
+        return self.FORMULA
+    def GetName(self):
+        return self.NAME
+
+someObj = Compound("Test",'GRAPH')
+secondobj = Compound("Test2","Graph2")
+
