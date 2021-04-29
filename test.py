dad60a4b3722d4530497554ec178042b4167aadd
Andrew Vlasov <captaindrewsa@gmail.com>
2021-04-29 16:07:55 +0200

    Test Classes without process

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import time
+
 class Compound():
 
     def __init__(self, name, graph):
@@ -8,4 +10,62 @@ class Compound():
         return self.FORMULA
     def GetName(self):
         return self.NAME
+
+class Protein():
+    NAME = ""
+    WORKFLOW = []
+    PARENT_COMPARTMENT = ""
+    
+    def __init__(self, name, parcomp):
+        self.NAME = name
+        self.PARENT_COMPARTMENT = parcomp
+
+    def GetInputSubstance(self):
+        pass
+
+    def Function(self, *args):
+        pass
+
+    def GiveOutputSubstance(self):
+        pass
+
+
+
+
+
+class Compartment():
+    NAME = ""
+    COMPONENTS = {"Proteins":[],"Compounds":[],"Compartments":[]}
+    MEM_COMP = {"Proteins":[],"Compounds":[]}
+
+    def SetComponents(self, **kwargs):
+        for types,item in kwargs.items():
+            if types=="Proteins":
+                self.COMPONENTS["Proteins"].append(item)
+            if types=="Compounds":
+                self.COMPONENTS["Compounds"].append(item)
+            if types=="Compartments":
+                self.COMPONENTS["Compartments"].append(item)
+    
+    def SetMemComponents(self, **kwargs):
+        for types,item in kwargs.items():
+            if types=="Proteins":
+                self.COMPONENTS["Proteins"].append(item)
+            if types=="Compounds":
+                self.COMPONENTS["Compounds"].append(item)
+    
+    def GetComponents(self):
+        return [self.COMPONENTS, self.MEM_COMP]
+
+
+Comp1 = Compartment()
+ProteinsTest = Protein("TestPROT",Comp1)
+CompTest = Compound("Comp",3)
+
+Comp1.SetComponents(Proteins=[ProteinsTest],Compounds=[CompTest])
+
+
+while True:
+    
+
 
